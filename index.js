0e80fe65cff0a41175884a024156eb17b29f28fb
Nathan Hill <nateohill@gmail.com>
2018-05-21 02:25:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,12 +9,23 @@ function nestedTarget() {
 }
 
 function deepestChild() {
+<<<<<<< HEAD
   let node = document.querySelector('div#grand-node');
 
   while (node.children.length !== 0) {
     node = node.children[0];
   }
   return node;
+=======
+  const child = [...document.querySelectorAll('div#grand-node div')];
+  return child.length;
+  // for(let i = child.length; i > 0; i--) {
+  //   if(i === (((child.length - 1)/2) + 1)) {
+  //     return child[i].textContent;
+  //   }
+  // }
+  // return document.querySelector('#grand-node div div div div');
+>>>>>>> cc25b8fc03e2f995c2ac789cafe9bb029e492a3d
 }
 
 function increaseRankBy(n) {
